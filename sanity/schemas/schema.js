e5aad1588bd367411d1eccd3ce97d6aef7d93170
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-07-14 13:19:04 +0530

    Modifying the sanity CMS Schema

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,10 +5,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import collection from './collection'
+import creator from './creator'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -17,13 +15,7 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    post,
-    author,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
+    collection,
+    creator
   ]),
 })
