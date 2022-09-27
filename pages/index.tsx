382dc2becc4433457a6ab6fdca67cbec3215d9a4
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-09-27 14:50:40 +0530

    chor: updating the NFts & tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,59 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { urlFor } from '../lib/sanity';
+import { sanityClient } from '../lib/sanity.server';
+import { groq } from 'next-sanity';
+import { Collection } from '../typings';
+import Link from 'next/link';
 
-const Home: NextPage = () => {
+interface Props {
+  collections: Collection[];
+}
+
+const Home = ({ collections }: Props) => {
+  console.log('COLLECTION:');
+  console.log(collections);
+
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto flex flex-col min-h-screen py-20 px-10 2xl:px-0">
       <Head>
-        <title>Meta NFT Drop</title>
+        <title>NFT Drop Challenge</title>
+        <meta name="description" content="NFT Drop Challenge" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1 className="mb-10 text-4xl font-extralight">
-        <span className="font-extrabold underline decoration-pink-600/50">{`Ritesh's `}</span>
+        <span className="font-extrabold underline decoration-pink-600/50">{`Tushar's `}</span>
         {` NFT Market Place`}
       </h1>
 
-     
+      <main className="bg-slate-100 p-10 shadow-xl shadow-rose-400/20">
+        <div className="grid space-x-3 md:grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+          {collections.map((collection) => (
+            <Link key={collection._id} href={`/nft/${collection.slug.current}`}>
+              <div className="flex flex-col items-center cursor-pointer transition-all duration-200 hover:scale-105">
+                <img
+                  className="h-96 w-60 rounded-2xl object-cover"
+                  src={urlFor(collection.mainImage).url()}
+                  alt={collection.nftCollectionName}
+                />
 
-      
+                <div className="p-5">
+                  <h2 className="text-3xl">{collection.title}</h2>
+                  <p className="mt-2 text-sm text-gray-400">
+                    {collection.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const query = groq`*[_type == "collection"] {
@@ -58,4 +88,3 @@ export const getServerSideProps: GetServerSideProps = async () => {
       collections,
     },
   };
-};
